b9a335c4399806d0b1cb2ee361ff0544f2a229b2
Your Name <kiennahe130771@fpt.edu.vn>
2021-04-19 15:29:21 +0700

    hw chapter1

---
+++ chapter1_hw.js
@@ -0,0 +1,15 @@
+//1
+let message = "Coding is great", studentCount = 22;
+console.log(message,'\n', studentCount);
+studentCount++;
+console.log(message.toLowerCase(),'\n', studentCount);
+//2
+alert("Ngày hôm nay đẹp quá!");
+//3
+let firstName = prompt("Họ của bạn là gì?"), lastName = prompt("Tên của bạn là gì?");
+alert(`Xin chào ${firstName} ${lastName}`);
+//4
+let length = (Number)(prompt("Chiều dài HCN là:"));
+let width = (Number)(prompt("Chiều rộng HCN là:"));
+alert(`Diện tích HCN là: ${length * width}`);
+
